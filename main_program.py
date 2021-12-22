2442e332052dfacef2e7997e76b79236041d0a34
yanak <yanakasaeva2006@gmail.com>
2021-12-22 17:15:09 +0300

    appearance fix 1

--- main_program.py
+++ main_program.py
@@ -1,6 +1,5 @@
 import pygame
 
-
 SPACE_PRESS_COUNT = 0
 
 
@@ -46,14 +45,57 @@ class Board:
             self.on_click(cell)
 
 
-def draw(screen):
+def draw_1():
     screen.fill((0, 0, 0))
-    font = pygame.font.Font(None, 50)
-    text = font.render("Морской бой", True, (0, 255, 255))
-    # нужно будет написать здесь правила игры (думаю, это можно из ТЗ скопировать)
-    text_x = width // 2 - text.get_width() // 2
-    text_y = height // 10 - text.get_height() // 10
-    screen.blit(text, (text_x, text_y))
+    font_1 = pygame.font.Font(None, 50)
+    font_2 = pygame.font.Font(None, 35)
+    rules = ['Морской бой', 'Нажмите "пробел", чтобы начать игру',
+             'Игра рассчитана на двух человек, играющих на одном устройстве', 'Первым расставляет корабли PLAYER 1',
+             'Чтобы это сделать, нужно нажать на корабль,', 'а затем на клетки поля, куда вы хотите его поместить',
+             'Чтобы зафиксировать местоположение корабля, нажмите "W"', 'После расстановки кораблей, нажмите "пробел"',
+             'Первым ходит PLAYER 1',
+             'Чтобы сделать ход, нажмите на клетку, куда вы хотите выстрелить', 'После победы нажмите "пробел",',
+             'чтобы увидеть результаты игры']
+    y = 15
+    for elem in rules:
+        if elem == 'Морской бой':
+            text = font_1.render(elem, True, (0, 255, 255))
+        else:
+            text = font_2.render(elem, True, (255, 255, 255))
+        text_x = width // 2 - text.get_width() // 2
+        text_y = height // y - text.get_height() // y
+        # нужно красиво расположить текст
+        screen.blit(text, (text_x, text_y))
+        y -= 8
+
+
+def draw_2():
+    screen.fill((0, 0, 0))
+    font = pygame.font.Font(None, 46)
+    letters = 'абвгдежзик'  # 'АБВГДЕЖЗИК'
+    letters_x_1 = 12
+    letters_x_2 = 408
+    letters_y = 88
+    for elem in letters:
+        text = font.render(elem, True, (255, 255, 255))
+        screen.blit(text, (letters_x_1, letters_y))
+        screen.blit(text, (letters_x_2, letters_y))
+        letters_y += 35
+    text = font.render("1  2  3  4  5  6  7  8  9 10", True, (255, 255, 255))
+    screen.blit(text, (55, 55))
+    screen.blit(text, (450, 55))
+    font_names = pygame.font.SysFont('arial', 50)
+    name_1 = font.render("PLAYER 1", True, (0, 255, 255))
+    name_2 = font.render("PLAYER 2", True, (0, 255, 255))
+    screen.blit(name_1, (55, 10))
+    screen.blit(name_2, (450, 10))
+
+    board1 = Board(10, 10)
+    board1.set_view(45, 85, 35)
+    board1.render(screen)
+    board2 = Board(10, 10)
+    board2.set_view(440, 85, 35)
+    board2.render(screen)
 
 
 if __name__ == '__main__':
@@ -61,7 +103,7 @@ if __name__ == '__main__':
     size = width, height = 800, 600
     screen = pygame.display.set_mode(size)
     pygame.display.set_caption("Касаева Яна; Цыганова Виктория")
-    draw(screen)
+    draw_1()
     running = True
     while running:
         for event in pygame.event.get():
@@ -69,14 +111,6 @@ if __name__ == '__main__':
                 running = False
             if (event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE) and SPACE_PRESS_COUNT == 0:
                 SPACE_PRESS_COUNT += 1
-                screen.fill('black')
-                board1 = Board(10, 10)
-                board1.set_view(45, 85, 35)
-                board1.render(screen)
-                board2 = Board(10, 10)
-                board2.set_view(440, 85, 35)
-                board2.render(screen)
-                # по краям расположить буквы и цифры; наверху - player1, player2
+                draw_2()
         pygame.display.flip()
     pygame.quit()
-
